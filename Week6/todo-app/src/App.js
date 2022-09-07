638fd2234c37a2685d0bb0fcd0040a489dbbe7be
William Sawyer <wmsawyer2609@gmail.com>
2022-09-07 12:35:56 +1000

    Created read-only todo application

--- Week6/todo-app/src/App.js
+++ Week6/todo-app/src/App.js
@@ -1,8 +1,33 @@
+const initialTodos = [
+    { task: "Take out the trash", completed: false },
+    { task: "Walk the dog", completed: true },
+    { task: "Do the weekly quizzes", completed: false },
+];
+
+function Todo({ task, completed }) {
+    return (
+        <section>
+            <input type="checkbox" checked={completed} />
+            <p style={{ display: "inline-block" }}>
+                {task}
+            </p>
+        </section>
+    );
+}
+
 function App() {
     return (
-        <header>
-            <h1>Hello React Web!</h1>
-        </header>
+        <>
+            <header>
+                <h1>Todo App</h1>
+            </header>
+            <main>
+                <h2>List of Todos</h2>
+                {initialTodos.map(todo => (
+                    <Todo key={todo.task} {...todo} />
+                ))}
+            </main>
+        </>
     );
 }
 
