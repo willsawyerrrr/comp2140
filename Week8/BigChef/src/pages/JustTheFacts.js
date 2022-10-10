747d4533921e9782e8989910d7125485c433e0ec
William Sawyer <wmsawyer2609@gmail.com>
2022-10-10 21:06:48 +1000

    Implemented just the facts page

--- Week8/BigChef/src/pages/JustTheFacts.js
+++ Week8/BigChef/src/pages/JustTheFacts.js
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
+
 import Template from "../components/Template";
 
+function Fact({ fact }) {
+    return <p>{fact}</p>;
+}
+
 export default function JustTheFacts() {
+    async function getFact() {
+        const response = await fetch("https://catfact.ninja/fact");
+        const data = await response.json();
+        const fact = data.fact;
+        setFact(fact);
+    }
+
+    useEffect(() => {
+        getFact();
+    }, []);
+
+    const initialFact = "Loading fact...";
+    const [fact, setFact] = useState(initialFact);
+
     return (
         <Template title="Just the Facts">
-            Just the Facts coming soon!
+            <h3>Random Fact</h3>
+            <Fact fact={fact} />
         </Template>
     );
+
 }
