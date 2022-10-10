e5d7ef141ac62c5bafb055a72ff6547a62c1a3fb
William Sawyer <wmsawyer2609@gmail.com>
2022-10-10 18:14:27 +1000

    Moved App to top level

--- Week7/BigChef/src/App.js
+++ Week7/BigChef/src/App.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Template from "../components/Template";
+import Template from "./components/Template";
 
 export default function App() {
     return (
--- Week7/BigChef/src/index.js
+++ Week7/BigChef/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./pages/App";
+import App from "./App";
 import Recipe from "./pages/Recipe";
 import RecipeList from "./pages/RecipeList";
 
